6ba41df88942ecfe5d969df8db451703fd62e266
Tim Kirves <tim.kirves@gmail.com>
2021-11-07 23:29:40 +0200

    initial commit

---
+++ demo-assetManagement/src/main/java/com/example/assetmanagement/demoassetManagement/DemoAssetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.assetmanagement.demoassetManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAssetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAssetManagementApplication.class, args);
+	}
+
+}
---
+++ demo-assetManagement/src/test/java/com/example/assetmanagement/demoassetManagement/DemoAssetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assetmanagement.demoassetManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAssetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
